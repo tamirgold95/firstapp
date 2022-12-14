24a40f33736778da6e4d597ab1181bc84614c9d8
Tamir Goldman <tamir.goldman@openweb.com>
2022-12-14 10:30:15 +0200

    To do list with option to remove tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Input from './components/Input';
+import Div from './components/Div'; 
 
 function App() {
+  const [userName,setUserName] = useState("Tamir")
+  const [inputValue, setInputValue] = useState("")
+  const [counter, Setcounter] = useState(0)
+  const [tasks, setTasks] = useState([])
+  const [checkedTask, setCheckesTask] = useState([]);
+  
+  const onInputChange = (value) => {
+    setInputValue(value)
+  }
+
+  const onAddTaskClick = () => {
+    if (inputValue === '') {
+      alert("Please insert some task");
+      return
+    }
+    for (let task of tasks) {
+      if (task == inputValue) {
+        alert("You already added that task.");
+        setInputValue("");
+        return;
+      }
+    }
+    setTasks(tasks => [...tasks, [inputValue]]
+      // return tasks
+    )
+    console.log(tasks);
+    setInputValue("");
+    Setcounter(counter + 1);
+  }
+
+  const onClickRemove = () => {
+    //console.log(tasks);
+    //console.log(checkedTask);
+    setTasks(tasks.filter(function (task) {
+      return !checkedTask.some(function (checkedTask) {
+        return task[0] === checkedTask;
+      });
+    }))
+    Setcounter(counter - checkedTask.length);
+    setCheckesTask([]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{userName}'s To Do List</h1>
+      <Input onInputChange={onInputChange} value={inputValue} /> 
+      <button onClick={onAddTaskClick}>Add Task</button>
+      <Div tasks={tasks} checkedTask={checkedTask} setCheckesTask={setCheckesTask} counter={counter} />
+      <button onClick={onClickRemove}>Clear Selected Tasks</button>
     </div>
   );
 }
